82fc68599bd45b677c7ad6e8e6c8622416a418d7
Kevin Massol <kevin.massol@icloud.com>
2023-02-08 11:21:01 +0100

    Initial Commit

---
+++ ILoveSantorini/ILoveSantoriniApp.swift
@@ -0,0 +1,17 @@
+//
+//  ILoveSantoriniApp.swift
+//  ILoveSantorini
+//
+//  Created by KEVIN on 08/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct ILoveSantoriniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
